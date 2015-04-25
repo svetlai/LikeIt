e16ba0089d3c786c9e57499ffcc229469f3fd395
svetlai <svetla_22@yahoo.com>
2015-04-25 20:47:23 +0300

    References and test project edited.

--- LikeIt/Tests/LikeIt.Web.Tests/Controllers/HomeControllerTest.cs
+++ LikeIt/Tests/LikeIt.Web.Tests/Controllers/HomeControllerTest.cs
@@ -1,54 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LikeIt.Web;
-using LikeIt.Web.Controllers;
-
-namespace LikeIt.Web.Tests.Controllers
+﻿namespace LikeIt.Web.Tests.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using LikeIt.Web;
+    using LikeIt.Web.Controllers;
+    using LikeIt.Data.Contracts;
+
     [TestClass]
     public class HomeControllerTest
     {
+        private ILikeItData data;
+
         [TestMethod]
         public void Index()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            //// Arrange
+            //HomeController controller = new HomeController(data);
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+            //// Act
+            //ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
+            //// Assert
+            //Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public void About()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            //// Arrange
+            //HomeController controller = new HomeController(data);
 
-            // Act
-            ViewResult result = controller.About() as ViewResult;
+            //// Act
+            //ViewResult result = controller.About() as ViewResult;
 
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            //// Assert
+            //Assert.AreEqual("Your application description page.", result.ViewBag.Message);
         }
 
         [TestMethod]
         public void Contact()
         {
-            // Arrange
-            HomeController controller = new HomeController();
+            //// Arrange
+            //HomeController controller = new HomeController(data);
 
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            //// Act
+            //ViewResult result = controller.Contact() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
+            //// Assert
+            //Assert.IsNotNull(result);
         }
     }
 }
